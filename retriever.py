413a4cbb4d5466c8db1f06a651a9e7f66b9c5f2d
Steosumit <steosumit@gmail.com>
2025-12-06 19:54:03 +0530

    refactor: minor changes to help in debugging

--- retriever.py
+++ retriever.py
@@ -6,7 +6,7 @@ This file contains the RAG retriever function components
 4. store the embeddings in a vector storage
 
 Keep reading the doc as we go along:
-https://docs.langchain.com/oss/python/integrations/document_loaders/unstructured_file
+https://docs.langchain.com/oss/python/integrations/document_loaders/
 """
 
 import os
@@ -14,13 +14,13 @@ from langchain_community.document_loaders.csv_loader import CSVLoader
 from langchain_core.runnables import RunnablePassthrough
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_huggingface import HuggingFaceEmbeddings
-from langchain_core.vectorstores import InMemoryVectorStore
+from langchain_core.vectorstores import InMemoryVectorStore, VectorStoreRetriever
 from dotenv import load_dotenv
 
 load_dotenv()  # load global env vars
 
 # main class: to organize all the functions
-class custom_retriever():
+class CustomRetriever():
     """This class is the main class to arrange the functions"""
 
     def __init__(self, file_path: str):
@@ -33,7 +33,7 @@ class custom_retriever():
             print("/data already exists")
         self.file_path = r"D:\Work\Projects\SteoNoteGent\data\sample.csv"  # file path list
         self.storage = None  # storage for documents objects
-        self. retreiver = None  # retriever object
+        self. retriever = None  # retriever object
     # loader: create the loader function as the RAG retriever
     def load(self):
         loader = CSVLoader(self.file_path)
@@ -52,27 +52,29 @@ class custom_retriever():
         documents_splits = text_splitter.split_documents(self.storage)
         print(f"[MSG] Number of chunks created: {len(documents_splits)}")
         print("[MSG] Sample chunk:")
+        print("===")
         print(documents_splits[0])
-        self.storage = documents_splits
+        print("===")
+        self.storage = documents_splits  # why using common storage? Saves memory plus the emdedding are memory based
 
     # Embed: create embeddings of the chunks (self.storage) and store the vectors
     def embed_storage(self):
         embeder = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
         vectorstore = InMemoryVectorStore(embedding=embeder)  # ony embedder is needed as it is in memory
         vectorstore.add_documents(self.storage)
-        self.retreiver = vectorstore.as_retriever(
+        self.retriever = vectorstore.as_retriever(
             search_type="similarity",
             search_kwargs={"k": 4}
         )
-    def run(self):
+    def run(self)-> VectorStoreRetriever:
         self.load()
         self.split()
         self.embed_storage()
-        return self.retreiver
+        return self.retriever
 
 # test code
 if __name__ == "__main__":
-    retriever_instance = custom_retriever("sample_data.csv")
+    retriever_instance = CustomRetriever("sample_data.csv")
     retriever = retriever_instance.run()
     print("[MSG] Retriever is ready to use.")
     # test the retriever
